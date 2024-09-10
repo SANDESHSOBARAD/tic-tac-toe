a886bcc7abb80782a6a95f53adf30cb9fc852ff2
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2024-09-10 12:25:33 -0700

    Game ready to play

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let boxes = document.querySelectorAll('.box')
-
+let newGame = document.querySelector('#newGame-btn')
+let winnerContainer = document.querySelector('.winner-container')
 let reset = document.querySelector('#reset-btn')
+let msg = document.querySelector('#message')
+
 
 let turnO = true;   //playerX, playerO
 
@@ -15,6 +18,14 @@ let winning_patterns = [
     [2,4,6]
 
 ]
+
+const playNewGame = () =>{
+    msg.innerText = ''
+    resetGame()
+
+}
+
+newGame.addEventListener('click', () => playNewGame())
 
 boxes.forEach( (box) =>{
     box.addEventListener('click', () =>{
@@ -31,14 +42,32 @@ boxes.forEach( (box) =>{
     })
 })
 
-    reset.addEventListener('click', () =>{
-        boxes.forEach((box) =>{
-            box.innerHTML = ''
-            turnO = true
-            box.disabled = false
-        })
-    })
+const resetGame = () => {
+        turnO = true;
+        enableBoxes()
+}
 
+reset.addEventListener('click', () => resetGame())
+
+const enableBoxes = () =>{
+    boxes.forEach((box) =>{
+        box.disabled = false
+        box.innerText = ''
+        winnerContainer.classList.add('hide')
+    })
+}
+
+const disableBoxes = () =>{
+    boxes.forEach((box) =>{
+        box.disabled = true
+    })
+}
+
+const showWinner = (winner) =>{
+    msg.innerText = `Congrats, Winner is player-${winner}`;
+    winnerContainer.classList.remove('hide')
+    disableBoxes()
+}
 
 const checkWinner = () =>{
     winning_patterns.forEach((pattern) =>{
@@ -47,15 +76,9 @@ const checkWinner = () =>{
         let pos3 = boxes[pattern[2]].innerText
 
         if (pos1 != "" && pos2 != "" && pos3 != ""){
-            if (pos1 === 'X' && pos2 === 'X' && pos3 === 'X'){
-                console.log("PlayerX is the winner")
-            }
-            else if (pos1 === 'O' && pos2 === 'O' &&  pos3 === 'O'){ 
-                console.log("PlayerO is the winner")
+            if (pos1 === pos2 && pos2 === pos3){
+                showWinner(pos1)
             }
         }
     })
-
-    
-
 }
